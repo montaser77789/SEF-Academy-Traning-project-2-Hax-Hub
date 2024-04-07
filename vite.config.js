826451bc656dcc8d9ca889577dea6a414d1ad710
montasergohar <montasergohar@gmail.com>
2024-04-07 05:17:19 +0200

    Error

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['@fortawesome/fontawesome-svg-core'],
-  },
-})
 // vite.config.js
 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@fortawesome/fontawesome-svg-core'],
+    },
+  },
+});
