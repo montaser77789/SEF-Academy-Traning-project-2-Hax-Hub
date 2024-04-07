8d6fa38f0edb6225a9c3e7de42a36e83a3725129
montasergohar <montasergohar@gmail.com>
2024-04-07 05:14:18 +0200

    Error 2 in Deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@fortawesome/fontawesome-svg-core'],
+  },
 })
+// vite.config.js
+
+
+
